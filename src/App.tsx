567a05339cfdd5e303685ef69ce6a6fcd2f922da
Joshua Daniels <joshuadaniels537@gmail.com>
2023-07-16 10:13:04 +0200

    feature: addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [display, setDisplay] = useState("");
+  const [output, setOutput] = useState(0);
+  const numbers = [
+    { display: "1", value: 1 },
+    { display: "2", value: 2 },
+    { display: "3", value: 3 },
+    { display: "4", value: 4 },
+    { display: "5", value: 5 },
+    { display: "6", value: 6 },
+    { display: "7", value: 7 },
+    { display: "8", value: 8 },
+    { display: "9", value: 9 },
+    { display: "0", value: 0 },
+  ];
+
+  function handleNumberClick(numberDisplay: string) {
+    setDisplay((display) => display + numberDisplay);
+  }
+
+  const plus = "+";
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p>{display}</p>
+        {numbers.map((number) => (
+          <button onClick={() => handleNumberClick(number.display)}>
+            {number.display}
+          </button>
+        ))}
+
+        <button
+          onClick={() => handleNumberClick(plus)}
+          type="button"
+          className="btn btn-primary"
+        >
+          {plus}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+        <button
+          onClick={() => setDisplay("")}
+          type="button"
+          className="btn btn-danger"
+        >
+          Clear
+        </button>
+
+        <button
+          onClick={() => setOutput(eval(display))}
+          type="button"
+          className="btn btn-warning"
+        >
+          =
+        </button>
+
+        <p>{output}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
